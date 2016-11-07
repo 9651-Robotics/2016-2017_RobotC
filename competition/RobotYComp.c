173184dd47544c4f4f40d7fbe6632b16c468af21
9651Robotics <9651robotics@gmail.com>
2016-11-07 17:35:37 -0500

    More work on RobotY Comp
    Everything works except thatthe value will not read once it has been
    zeroed

--- competition/RobotYComp.c
+++ competition/RobotYComp.c
@@ -1,3 +1,4 @@
+#pragma config(Sensor, in1, armAngle, sensorPotentiometer)
 #pragma config(Sensor, dgtl4,  sonarRight,     sensorSONAR_cm)
 #pragma config(Sensor, dgtl6,  sonarLeft,      sensorSONAR_cm)
 #pragma config(Sensor, dgtl8,  sonarBack,      sensorSONAR_cm)
@@ -21,7 +22,7 @@
 
 #include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
 
-
+int armDegree =SensorValue(armAngle);
 
 void pre_auton()
 {
@@ -36,7 +37,17 @@ void pre_auton()
 
 task autonomous()
 {
-	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
+        //motor[armRight1] = -127;
+   	 		//motor[armLeft1] = -127;
+   	 		//motor[armRight2] = -127;jjuud
+    		//motor[armLeft2] = -127;
+
+
+    		//motor[armRight1] = 0;
+   	 		//motor[armLeft1] = 0;
+   	 		//motor[armRight2] = 0;
+    		//motor[armLeft2] = 0;
+
 }
 
 
@@ -51,16 +62,16 @@ task usercontrol(){
     	motor[backLeft] =  vexRT[Ch3] + vexRT[Ch1] - vexRT[Ch4];
 
     	if(vexRT[Btn6U] == 1) {
-    		motor[armRight1] = 32;
-    		motor[armLeft1] = 32;
-    		motor[armRight2] = 32;
-    		motor[armLeft2] =  32;
+    		motor[armRight1] = 70;
+    		motor[armLeft1] = 70;
+    		motor[armRight2] = 70;
+    		motor[armLeft2] =  70;
    	 	}
    	 	else if(vexRT[Btn6D] == 1) {
-   	 		motor[armRight1] = 127;
-   	 		motor[armLeft1] = 127;
-   	 		motor[armRight2] = 127;
-    		motor[armLeft2] = 127;
+   	 		motor[armRight1] = -90;
+   	 		motor[armLeft1] = -90;
+   	 		motor[armRight2] = -90;
+    		motor[armLeft2] = -90;
    		}
    		else {
    			motor[armRight1] = 0;
@@ -74,34 +85,45 @@ task usercontrol(){
 					clearLCDLine(0);
   				clearLCDLine(1);
 			}
+			else if(vexRT[Btn7U] == 1){
+				SensorValue(armAngle) = 0;
+				armDegree = 0;
+				clearLCDLine(0);
+  			clearLCDLine(1);
+			}
 			else if(vexRT[Btn5D] == 1)
 			{
 				bLCDBacklight = true;
 			}
 			else
+
 			{
 				bLCDBacklight = false;
 			}
 
-  	displayLCDPos(0,0);
-  	displayNextLCDString("F:");
- 		displayNextLCDNumber(SensorValue(sonarFront));
- 		displayNextLCDString("CM");
+		displayLCDPos(0,0);
+		displayNextLCDString("Arm:");
+		displayNextLCDNumber(armDegree);
 
- 		displayLCDPos(0,9);
- 		displayNextLCDString("B:");
- 		displayNextLCDNumber(SensorValue(sonarBack));
- 		displayNextLCDString("CM");
+  	//displayLCDPos(0,0);
+  	//displayNextLCDString("F:");
+ 		//displayNextLCDNumber(SensorValue(sonarFront));
+ 		//displayNextLCDString("CM");
 
- 		displayLCDPos(1,0);
- 		displayNextLCDString("L:");
- 		displayNextLCDNumber(SensorValue(sonarLeft));
- 		displayNextLCDString("CM");
+ 		//displayLCDPos(0,9);
+ 		//displayNextLCDString("B:");
+ 		//displayNextLCDNumber(SensorValue(sonarBack));
+ 		//displayNextLCDString("CM");
 
- 		displayLCDPos(1,9);
- 		displayNextLCDString("R:");
- 		displayNextLCDNumber(SensorValue(sonarRight));
- 		displayNextLCDString("CM");
+ 		//displayLCDPos(1,0);
+ 		//displayNextLCDString("L:");
+ 		//displayNextLCDNumber(SensorValue(sonarLeft));
+ 		//displayNextLCDString("CM");
+
+ 		//displayLCDPos(1,9);
+ 		//displayNextLCDString("R:");
+ 		//displayNextLCDNumber(SensorValue(sonarRight));
+ 		//displayNextLCDString("CM");
 
 	}
 }
