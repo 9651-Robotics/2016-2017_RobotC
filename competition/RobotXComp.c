97fd9dc4308ebfd89b1fad1a4ff8e962934bd76c
9651Robotics <9651robotics@gmail.com>
2016-11-10 17:17:44 -0500

    Added rotation notation

--- competition/RobotXComp.c
+++ competition/RobotXComp.c
@@ -9,6 +9,7 @@
 #pragma config(Motor,  port4,           driveMiddle,   tmotorVex393, openLoop)
 #pragma config(Motor,  port5,           armRight,      tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
 #pragma config(Motor,  port6,           armLeft,       tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
+#pragma config(Motor,  port7,           armFork,   tmotorVex393, openLoop)
 
 #pragma platform(VEX)
 
@@ -22,6 +23,9 @@
 int nBatteryAverage = nAvgBatteryLevel;
 
 //const float rotations = 360.0;
+//Rotations captured @ 127 motor power
+//Ticks per one wheel rotation back left = 362, 352, 379, 369, 360, 364, 363, 355
+//Ticks per one wheel roation back right = 373, 343, 371, 377, 363, 350, 369, 359
 
 void pre_auton()
 {
@@ -54,29 +58,40 @@ task usercontrol()
 
 		motor[driveRight] = (vexRT[Ch3]);
 		motor[driveLeft] = (vexRT[Ch2]);
-		while(vexRT[Btn7U] == 1)
+
+		while(vexRT[Btn8U] == 1)
 		{
 				bLCDBacklight = true;
 		}
-		if(vexRT[Btn6D] == 1) {
+		if(vexRT[Btn8R] == 1) {
 			motor[driveMiddle] = -127;
+		}
+		else if(vexRT[Btn8L] == 1)
+		{
+			motor[driveMiddle] = 127;
+		}
+		else if(vexRT[Btn5U] == 1)
+		{
+			motor[armFork] = -127;
 		}
 		else if(vexRT[Btn5D] == 1)
 		{
-			motor[driveMiddle] = 127;
+			motor[armFork] = 127;
+		}
+		else if(vexRT[Btn6D] == 1)
+		{
+			motor[armRight] = -127;
+			motor[armLeft] = -127;
 		}
 		else if(vexRT[Btn6U] == 1)
 		{
 			motor[armRight] = 127;
 			motor[armLeft] = 127;
 		}
-		else if(vexRT[Btn5U] == 1)
+		else if(vexRT[Btn8D] == 1)
 		{
-			motor[armRight] = -127;
-			motor[armLeft] = -127;
-		}
-		else if(vexRT[Btn8U] == 1)
-		{
+			clearLCDLine(0);
+  		clearLCDLine(1);
 			SensorValue[rightEncoder] = 0;
 			SensorValue[leftEncoder] = 0;
 		}
@@ -85,6 +100,8 @@ task usercontrol()
 			motor[armRight] = 0;
 			motor[armLeft] = 0;
 			motor[driveMiddle] = 0;
+			motor[armFork] = 0;
+			bLCDBacklight = false;
 		} //Close else
 	} // Close true loop brace
 } // Close user control brace
