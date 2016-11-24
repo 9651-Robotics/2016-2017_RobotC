25206ace26f795d2a688d8053818ab5178dc79b2
9651Robotics <9651robotics@gmail.com>
2016-11-24 15:58:46 -0500

    Frisco.c is now RobotX.c
    We can now delete Frisco.c

--- competition/RobotXComp.c
+++ competition/RobotXComp.c
@@ -10,7 +10,7 @@
 #pragma config(Motor,  port4,           driveMiddle,   tmotorVex393, openLoop)
 #pragma config(Motor,  port5,           armRight,      tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
 #pragma config(Motor,  port6,           armLeft,       tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
-#pragma config(Motor,  port7,           armFork,   tmotorVex393, openLoop)
+#pragma config(Motor,  port7,           armFork,       tmotorVex393, openLoop)
 
 #pragma platform(VEX)
 
@@ -21,11 +21,49 @@
 
 #include "Vex_Competition_Includes.c"
 
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+
 int nBatteryAverage = nAvgBatteryLevel;
 
+//const float rotations = 360.0;
+//Rotations captured @ 127 motor power
+//Ticks per one wheel rotation back left = 362, 352, 379, 369, 360, 364, 363, 355
+//Ticks per one wheel roation back right = 373, 343, 371, 377, 363, 350, 369, 359
 //11.2 ticks = 1 CM of movement on the back wheels (both sides)
-//Wheel diameter = 10.3 CM
+//Wheel diameter = 10.3 CM (32.3584)
 
+
+void waitForPress() {
+	while(nLCDButtons == 0){}
+	wait1Msec(3);
+}
+void waitForRelease()
+{
+	while(nLCDButtons != 0){}
+	wait1Msec(3);
+}
+void armUp() {
+	motor[armRight] = 127;
+	motor[armLeft] = 127;
+}
+void armDown() {
+	motor[armRight] = -90;
+	motor[armLeft] = -90;
+}
+void forkDown() {
+	motor[armFork] = 25;
+}
+void forkUp() {
+	motor[armFork] = -50;
+}
+void middleRight() {
+	motor[driveMiddle] = -127;
+}
+void middleLeft() {
+	motor[driveMiddle] = 127;
+}
 
 void pre_auton()
 {
@@ -33,14 +71,38 @@ void pre_auton()
 	// Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
 	bStopTasksBetweenModes = true;
 
-	//SensorValue[rightEncoder] = 0;
+	SensorValue[rightEncoder] = 0;
+	SensorValue[leftEncoder] = 0;
+	nMotorEncoder[rightEncoder]=0;
+	nMotorEncoder[leftEncoder]=0;
+	SensorValue[dgtl1] = 0;
+	SensorValue[dgtl3] = 0;
 
 }
 
 task autonomous()
 {
+	  // If we do end up needing two autonomous modes, check back on (https://www.vexforum.com/index.php/10222-how-to-program-lcd-display-robotc/0)
 
-	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
+	  //Stage 1: turn right and left motors forward
+	  while(SensorValue[rightEncoder] <= 430 && SensorValue[leftEncoder] <= 430) {
+	  	motor[driveLeft] = 67;
+	  	motor[driveRight] = 67;
+	  }
+	  wait1Msec(1000);
+	  //Stage 2: turn 180 degrees
+	  // ISSUE
+	  while(SensorValue[rightEncoder] >= -167 && SensorValue[leftEncoder] <= 1110) {
+	  	motor[driveLeft] = 67;
+	  	motor[driveRight] = -67;
+	  }
+	  //Turn arm sometime between stages two and three
+	  wait1Msec(1000);
+	  //Stage 3: reverse
+	  while(SensorValue[rightEncoder] >= -1280 && SensorValue[leftEncoder] >= -20) {
+	  	motor[driveLeft] = -67;
+	  	motor[driveRight] = -67;
+	  }
 
 }
 
@@ -48,65 +110,82 @@ task usercontrol()
 {
 
 	string mainBattery, backupBattery;
+	int count = 0;
 
 	while (true) {
 
-		displayLCDPos(0,0);
-		displayNextLCDString("Left:");
-		displayNextLCDNumber(SensorValue(leftEncoder));
-		displayNextLCDString("_");
-		displayNextLCDString("Right:");
-		displayNextLCDNumber(SensorValue(rightEncoder));
-		displayLCDString(0, 0, "Battery: ");
-		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V');
+		//Display information
+		// Encoder values
+		// Battery potential differance
+		// Joystick values
 
+		//Attempt to define closer in the file to the void functions
+		int leftForce = vexRT[Ch2];
+		int rightForce = vexRT[Ch3];
+
+		//matthew wang more like matthew "WANG"
+
+														// - Carpet Cock
+		displayLCDPos(0,0);
+		displayNextLCDString("L:");
+		displayNextLCDNumber(SensorValue(leftEncoder));
+		displayNextLCDString("  ");
+		displayNextLCDString("R:");
+		displayNextLCDNumber(SensorValue(rightEncoder));
+		displayLCDString(1, 0, "Bat: ");
+		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V');
+		displayNextLCDString(mainBattery);
 
 		motor[driveRight] = (vexRT[Ch3]);
 		motor[driveLeft] = (vexRT[Ch2]);
 
 		while(vexRT[Btn8U] == 1)
 		{
-				bLCDBacklight = true;
+			bLCDBacklight = true;
 		}
-		if(vexRT[Btn8R] == 1) {
-			motor[driveMiddle] = -127;
-		}
-		else if(vexRT[Btn8L] == 1)
-		{
-			motor[driveMiddle] = 127;
-		}
-		else if(vexRT[Btn5U] == 1)
-		{
-			motor[armFork] = -127;
-		}
-		else if(vexRT[Btn5D] == 1)
-		{
-			motor[armFork] = 127;
-		}
-		else if(vexRT[Btn6D] == 1)
-		{
-			motor[armRight] = -127;
-			motor[armLeft] = -127;
-		}
-		else if(vexRT[Btn6U] == 1)
-		{
-			motor[armRight] = 127;
-			motor[armLeft] = 127;
-		}
-		else if(vexRT[Btn8D] == 1)
+		if(vexRT[Btn8D] == 1)
 		{
 			clearLCDLine(0);
   		clearLCDLine(1);
 			SensorValue[rightEncoder] = 0;
 			SensorValue[leftEncoder] = 0;
 		}
-		else
-		{
-			motor[armRight] = 0;
-			motor[armLeft] = 0;
+		else {
+
+		}
+
+		//Middle Wheel (Currently immobile due to arm weight)
+		if(vexRT[Btn8R] == 1) {
+			middleRight();
+		}
+		else if(vexRT[Btn8L] == 1) {
+			middleLeft();
+		}
+		else{
 			motor[driveMiddle] = 0;
-			motor[armFork] = 0;
-			bLCDBacklight = false;
-		} //Close else
-	} // Close true loop brace
-} // Close user control brace
+		}
+
+		//Fork (arm built onto the end of the main arm)
+		if(vexRT[Btn5U] == 1) {
+			forkUp();
+		}
+		else if(vexRT[Btn5D] == 1) {
+			forkDown();
+		}
+		else {
+			 motor[armFork] = 0;
+		}
+
+		//Arm motors
+		if(vexRT[Btn6U] == 1) {
+			armUp();
+		}
+		else if(vexRT[Btn6D] == 1) {
+			armDown();
+		}
+		else {
+			motor[armLeft] = 0;
+			motor[armRight] = 0;
+		}
+	}
+}
