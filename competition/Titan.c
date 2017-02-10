a9f8af2a123c9ca6e678693a31f21d25e2540de3
John Mace <john.mace@ucc.on.ca>
2017-02-10 11:37:57 -0500

    Minor changes to Titan.c

--- competition/Titan.c
+++ competition/Titan.c
@@ -9,62 +9,43 @@
 
 #pragma platform(VEX)
 
-//Competition Control and Duration Settings
 #pragma competitionControl(Competition)
 #pragma autonomousDuration(20)
 #pragma userControlDuration(120)
 
-#include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+#include "Vex_Competition_Includes.c"   
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                          Pre-Autonomous Functions
-//
-// You may want to perform some actions before the competition starts. Do them in the
-// following function.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
+const short leftButton = 1;
+const short centerButton = 2;
+const short rightButton = 4;
+
+int nBatteryAverage = nAvgBatteryLevel;
+
+void LowerAndOpen() {
+	motor[armRight] = 127;
+	motor[armLeft] = 127;
+}
+
+void UpAndClose() {
+	
+}
 
 void pre_auton()
 {
-  // Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
-  // Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
+
   bStopTasksBetweenModes = true;
 
-	// All activities that occur before the competition starts
-	// Example: clearing encoders, setting servo positions, ...
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                                 Autonomous Task
-//
-// This task is used to control your robot during the autonomous phase of a VEX Competition.
-// You must modify the code to add your own robot specific commands here.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
 
 task autonomous()
 {
-  // .....................................................................................
-  // Insert user code here.
-  // .....................................................................................
 
 	AutonomousCodePlaceholderForTesting();  // Remove this function call once you have "real" code.
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-//                                 User Control Task
-//
-// This task is used to control your robot during the user control phase of a VEX Competition.
-// You must modify the code to add your own robot specific commands here.
-//
-/////////////////////////////////////////////////////////////////////////////////////////
 
 task usercontrol()
 {
-	// User control code here, inside the loop
 
 	while (true)
 	{
