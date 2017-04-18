c82b94d98049369a84597ae6972fbb82c91b73ce
9651Robotics <9651robotics@gmail.com>
2017-04-18 16:55:37 -0400

    Stuff happened
    I wrote comments
    Mace wrote code

--- really fast things/Mark5.c
+++ really fast things/Mark5.c
@@ -7,18 +7,14 @@
 #pragma config(Sensor, dgtl3,  					FrontLeftEncoder,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl5,           BackRightEncoder,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl7,           BackLeftEncoder,    sensorQuadEncoder)
-#pragma config(Motor,  port2,           driveRight,    tmotorVex269, openLoop)
-#pragma config(Motor,  port3,           driveLeft,     tmotorVex269, openLoop)
-#pragma config(Motor,  port4,           driveMiddle,   tmotorVex393, openLoop)
-#pragma config(Motor,  port5,           armRight,      tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
-#pragma config(Motor,  port6,           armLeft,       tmotorVex393_HBridge, openLoop, encoderPort, I2C_2)
-#pragma config(Motor,  port7,           armFork,       tmotorVex393, openLoop)
+#pragma config(Motor,  port2,           driveRightFront,    tmotorVex269, openLoop)
+#pragma config(Motor,  port3,           driveLeftFront,     tmotorVex269, openLoop)
+#pragma config(Motor,  port4,           driveRightBack1,    tmotorVex393, openLoop)
+#pragma config(Motor,  port5,           driveRightBack2,    tmotorVex393, openLoop)
+#pragma config(Motor,  port6,           driveLeftBack1,     tmotorVex393, openLoop)
+#pragma config(Motor,  port7,           driveLeftBack2,     tmotorVex393, openLoop)
 
 #pragma platform(VEX)
-
-const short leftButton = 1;
-const short centerButton = 2;
-const short rightButton = 4;
 
 int nBatteryAverage = nAvgBatteryLevel;
 int FrontleftShaft = SensorValue[FrontLeftEncoder];
@@ -32,17 +28,13 @@ int shaftAvg = (SensorValue[FrontRightEncoder] + SensorValue[FrontLeftEncoder])/
 //11.2 ticks = 1 CM of movement on the back wheels (both sides)
 //Wheel diameter = 10.3 CM (32.3584)
 
-task usercontrol() {
 
 	string mainBattery, backupBattery;
 	int count = 0;
 
-	while (true) {
+task main() {
 
-		//Display information
-		// Encoder values
-		// Battery potential differance
-		// Joystick values
+	while (true) {
 
 		//Attempt to define closer in the file to the void functions
 		int leftForce = vexRT[Ch2];
@@ -64,57 +56,11 @@ task usercontrol() {
 		//sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V');
 		//displayNextLCDString(mainBattery);
 
-		motor[driveRight] = (vexRT[Ch3]);
-		motor[driveLeft] = (vexRT[Ch2]);
-
-		while(vexRT[Btn8U] == 1)
-		{
-			bLCDBacklight = true;
-		}
-
-		if(vexRT[Btn8D] == 1)
-		{
-			clearLCDLine(0);
-			clearLCDLine(1);
-			SensorValue[FrontRightEncoder] = 0;
-			SensorValue[FrontLeftEncoder] = 0;
-		}
-		else {
-
-		}
-
-		//Middle Wheel (Currently immobile due to arm weight)
-		if(vexRT[Btn8R] == 1) {
-			middleRight();
-		}
-		else if(vexRT[Btn8L] == 1) {
-			middleLeft();
-		}
-		else{
-			motor[driveMiddle] = 0;
-		}
-
-		//Fork (arm built onto the end of the main arm)
-		if(vexRT[Btn5U] == 1) {
-			forkUp();
-		}
-		else if(vexRT[Btn5D] == 1) {
-			forkDown();
-		}
-		else {
-			 motor[armFork] = 0;
-		}
-
-		//Arm motors
-		if(vexRT[Btn6U] == 1) {
-			armUp();
-		}
-		else if(vexRT[Btn6D] == 1) {
-			armDown();
-		}
-		else {
-			motor[armLeft] = 0;
-			motor[armRight] = 0;
-		}
+		motor[driveRightFront] = (vexRT[Ch3]);
+		motor[driveLeftFront] = (vexRT[Ch2]);
+		motor[driveRightBack1] = (vexRT[Ch3]);
+		motor[driveRightBack2] = (vexRT[Ch3]);
+		motor[driveLeftBack1] = (vexRT[Ch2]);
+		motor[driveLeftBack2] = (vexRT[Ch2]);
 	}
 }
