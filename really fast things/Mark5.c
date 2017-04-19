d8f3c2aeb14e7ae8a646b859c3c2cf9749ad9607
John Mace <john.mace@ucc.on.ca>
2017-04-19 09:57:31 -0400

    Changes to Mark 5

--- really fast things/Mark5.c
+++ really fast things/Mark5.c
@@ -3,8 +3,10 @@
 #pragma config(Sensor, in1, armAngle, sensorPotentiometer)
 #pragma config(Sensor, I2C_2,  , sensorQuadEncoderOnI2CPort, ,AutoAssign)
 #pragma config(Sensor, I2C_3,  , sensorQuadEncoderOnI2CPort, ,AutoAssign)
-#pragma config(Sensor, dgtl1,  					FrontRightEncoder,  sensorQuadEncoder)
-#pragma config(Sensor, dgtl3,  					FrontLeftEncoder,   sensorQuadEncoder)
+#pragma config(Sensor, in1,    					expander_1,    sensorAnalog)
+#pragma config(Sensor, in2,    					expander_2,    sensorAnalog)
+#pragma config(Sensor, dgtl1,  			FrontRightEncoder,  sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  			FrontLeftEncoder,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl5,           BackRightEncoder,   sensorQuadEncoder)
 #pragma config(Sensor, dgtl7,           BackLeftEncoder,    sensorQuadEncoder)
 #pragma config(Motor,  port2,           driveRightFront,    tmotorVex269, openLoop)
@@ -17,50 +19,76 @@
 #pragma platform(VEX)
 
 int nBatteryAverage = nAvgBatteryLevel;
-int FrontleftShaft = SensorValue[FrontLeftEncoder];
-int FrontrightShaft = SensorValue[FrontRightEncoder];
-int shaftAvg = (SensorValue[FrontRightEncoder] + SensorValue[FrontLeftEncoder])/2;
+int frontRightShaft = SensorValue[FrontRightEncoder];
+int frontLeftShaft = SensorValue[FrontLeftEncoder];
+int backRightShaft = SensorValue[BackRightEncoder];
+int backLeftShaft = SensorValue[BackLeftEncoder];
 
 //const float rotations = 360.0;
-//Rotations captured @ 127 motor power
-//Ticks per one wheel rotation back left = 362, 352, 379, 369, 360, 364, 363, 355
-//Ticks per one wheel roation back right = 373, 343, 371, 377, 363, 350, 369, 359
-//11.2 ticks = 1 CM of movement on the back wheels (both sides)
 //Wheel diameter = 10.3 CM (32.3584)
 
+//Adaprive Steering Blueprint
+// Run all motors for 5 seconds and record encoder ticks
+// Sort from hightst to lowest
+// Reduce motor power accordingly
 
-	string mainBattery, backupBattery;
-	int count = 0;
+string mainBattery, backupBattery;
+
+int count = 1;
 
 task main() {
 
 	while (true) {
 
-		//Attempt to define closer in the file to the void functions
-		int leftForce = vexRT[Ch2];
-		int rightForce = vexRT[Ch3];
 
-		displayLCDPos(0,0);
-		displayNextLCDString("L:");
-		displayNextLCDNumber(SensorValue(FrontLeftEncoder));
-		displayNextLCDString("  ");
-		displayNextLCDString("R:");
-		displayNextLCDNumber(SensorValue(FrontRightEncoder));
-		displayLCDPos(1,0);
-		displayNextLCDString("BL:");
-		displayNextLCDNumber(SensorValue(BackLeftEncoder));
-		displayNextLCDString("  ");
-		displayNextLCDString("BR:");
-		displayNextLCDNumber(SensorValue(BackRightEncoder));
-		//displayLCDString(1, 0, "Bat: ");
-		//sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V');
-		//displayNextLCDString(mainBattery);
+		bLCDBacklight = true;
+
+		if (count % 2 == 0)
+		{
+
+			clearLCDLine(0);
+		    clearLCDLine(1); 
+
+			displayLCDPos(0,0);
+			displayNextLCDString("L:");
+			displayNextLCDNumber(SensorValue(FrontLeftEncoder));
+			displayNextLCDString("  ");
+			displayNextLCDString("R:");
+			displayNextLCDNumber(SensorValue(FrontRightEncoder));
+			displayLCDPos(1,0);
+			displayNextLCDString("BL:");
+			displayNextLCDNumber(SensorValue(BackLeftEncoder));
+			displayNextLCDString("  ");
+			displayNextLCDString("BR:");
+			displayNextLCDNumber(SensorValue(BackRightEncoder));
+
+		}
+			
+		else
+
+		{
+
+			clearLCDLine(0);
+		    clearLCDLine(1); 
+
+			displayLCDString(0, 0, "Main: "); 
+			sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0, 'V'); 
+			displayNextLCDString(mainBattery);
+
+			displayLCDString(1, 0, "L: ");
+			sprintf(backupBattery, "%1.2f R: %1.2f", SensorValue(expander_1)/283.2, SensorValue(expander_2)/283.2); 
+			displayNextLCDString(backupBattery); 
+
+			wait1Msec(100);
+
+		}	
 
 		motor[driveRightFront] = (vexRT[Ch3]);
-		motor[driveLeftFront] = (vexRT[Ch2]);
+		motor[driveLeftFront]  = (vexRT[Ch2]);
 		motor[driveRightBack1] = (vexRT[Ch3]);
 		motor[driveRightBack2] = (vexRT[Ch3]);
-		motor[driveLeftBack1] = (vexRT[Ch2]);
-		motor[driveLeftBack2] = (vexRT[Ch2]);
+		motor[driveLeftBack1]  = (vexRT[Ch2]);
+		motor[driveLeftBack2]  = (vexRT[Ch2]);
+
 	}
 }
